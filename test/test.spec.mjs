d973c83afdad5fdc4a91a77136bc20032bcdbba2
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-06-13 16:06:33 -0700

    added exports, modules, updated gitignore

---
+++ test/test.spec.mjs
@@ -0,0 +1,23 @@
+import {
+    throwsAnError,
+    did,
+    does,
+    have,
+    has,
+    is,
+    matches,
+    getCounter,
+    expectToBeTrue,
+    expectValuesToMatch,
+    objectsAreEquivalent,
+    expectObjectsAreEqual,
+    expectToBeNull,
+    throwsError,
+    expectConstructorToThrowError,
+    nullCheck,
+} from '../module/chaitests/Chai.mjs'
+import { expect } from 'chai'
+
+
+
+
