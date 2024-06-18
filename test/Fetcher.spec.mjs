8776ce430b3f8f28d137843c03f4576203067d06
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-06-18 04:02:47 -0700

    removed submodule in $folder

--- test/Fetcher.spec.mjs
+++ test/Fetcher.spec.mjs
@@ -1,20 +1,15 @@
 import {
-    throwsAnError,
-    did,
-    does,
-    have,
-    has,
-    is,
-    matches,
-    getCounter,
     expectToBeTrue,
     expectValuesToMatch,
+    expectArraysToBeEqual,
+    expectArrayToInclude,
+    expectArraytoIncludeArrayContents,
     objectsAreEquivalent,
+    expectStringToInclude,
     expectObjectsAreEqual,
     expectToBeNull,
     throwsError,
-    expectConstructorToThrowError,
-    nullCheck,
+    expectConstructorToThrowError
 } from '../module/chaitests/Chai.mjs'
 import { 
     expect,
@@ -22,34 +17,16 @@ import {
 } from 'chai'
 import { ENV } from '../secret.mjs'
 import { Fetcher } from '../Fetcher.mjs'
-import { WeatherFetcher as Weather } from '../components/WeatherAPI.mjs'
-import { TrelloFetcher as Trello } from '../components/Trello.mjs'
 
 
 
-const w = new Weather()
 
-const t = new Trello()
-console.log('t.parameters', t.parameters)
-// const userData = await t.getUserData('all')
-// console.log('trello user data:', userData)
-const board = { id: '6671546fb65c8adb3129428a' }
-// const boardData = await t.getBoard(board.id)
-// console.log('board data:', boardData)
-const lists = {}
-// const boardLists = await t.getBoardLists(board.id)
-const list = { id: '' }
-// const listData = await t.getList(list.id)
-// const card = {
-//     name: 'test card',
-//     idList: ''
-// }
-// let cardData = await t.postCard()
-
+const f = new Fetcher(`https://${ENV.fetcherMock.token}.mockapi.io`)
+const userGetted = f.GET('/api/v1/users/0')
 
 describe('Fetcher.mjs',() => {
     describe('constructor', () => {
-        expectValuesToMatch(w.base_url, 'http://api.weatherapi.com/v1')
+        expectStringToInclude('subjectAlias', subject, 'targetAlias', target)
     })
 
     describe('getData()',() => {
