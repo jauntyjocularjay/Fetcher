543115c5114ba07e31bab915a41ca7647ea73e95
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-06-27 08:40:54 -0700

    started refactor of mockio

--- Components/Mockio.mjs
+++ Components/Mockio.mjs
@@ -1,57 +1,79 @@
-// import {
-//     Fetcher
-// } from '../Fetcher.mjs'
-// import { ENV } from '../secret.mjs'
+import {
+    Fetcher
+} from '../Fetcher.mjs'
+import { ENV } from '../secret.mjs'
 
-// class MockIOFetcher extends Fetcher {
-//     constructor(resourceSchemas=[]){
-//         super(`https//${ENV.mockio.token}.mockapi.io`)
-//         this.resources = {}
+class MockIOFetcher extends Fetcher {
+    constructor(resourceSchemas={}){
+        super(`https//${ENV.mockio.token}.mockapi.io`)
+        this.resources = {}
 
-//         this.setEndpoints('/api/v1', resourceSchemas)
-//     }
+        this.setEndpoints('/api/v1', resourceSchemas)
+    }
 
-//     setEndpoints(prefix, resourceSchemas){
-//         let endpointString = '' + prefix
-        
-//         resourceSchemas.forEach(resource => {
-//             const endpoint = {}
-//             this.#parseLayers(endpointString, resource, true)
-//             this.resources.push(endpoint)
-//         })
-//     }
+    setEndpoints(prefix, resourceSchemas){
+        let endpointString = '' + prefix
 
-//     #parseLayers(endpointString, layer, root=false){
-//         for(const [key, value] of Object.entries(layer)){
-//             if(SchemaType.typeMatches(value)){
-//                 return                
-//             } else {
+        for(const [key, value] of Object.entries(resourceSchemas)){
+            
+        }
+        /**
+ const resourceLayers = {
+    "user": {
+        "id": { "type": "number" },
+        "name": {
+            "first": { "type":"string" },
+            "last": { "type":"string" }
+        },
+        "task": { "type": "array" }
+    },
+    "task": {
+        "task_id": { "type": "number" },
+        "title": { "type":"string" },
+        "completed": { "type":"boolean" }
+    }
+}
+         */
+
+
+        // resourceSchemas.forEach(resource => {
+        //     const endpoint = {}
+        //     this.#parseLayers(endpointString, resource, true)
+        //     this.resources.push(endpoint)
+        // })
+    }
+
+    #parseLayers(endpointString, layer, root=false){
+        for(const [key, value] of Object.entries(layer)){
+            if(SchemaType.typeMatches(value)){
+                return                
+            } else {
                 
-//             }
-//         }
-//     }
+            }
+        }
+    }
 
-//     async GET(endpoint, obj={headers:{},parameters:{}}){
-//         return await super.GET(endpoint, obj)
-//     }
+    async GET(endpoint, obj={headers:{},parameters:{}}){
+        return await super.GET(endpoint, obj)
+    }
 
-//     async PUT(endpoint, body, obj={headers:{},parameters:{}, body:{}}){
-//         return await super.PUT(endpoint, obj)
-//     }
+    async PUT(endpoint, body, obj={headers:{},parameters:{}, body:{}}){
+        return await super.PUT(endpoint, obj)
+    }
 
-//     async POST(endpoint, body, obj={headers:{},parameters:{}, body:{}}){
-//         return await super.POST(endpoint, obj)
-//     }
+    async POST(endpoint, body, obj={headers:{},parameters:{}, body:{}}){
+        return await super.POST(endpoint, obj)
+    }
 
-//     async PATCH(endpoint, body, obj={headers:{},parameters:{}, body:{}}){
-//         return await super.PATCH(endpoint, obj)
-//     }
+    async PATCH(endpoint, body, obj={headers:{},parameters:{}, body:{}}){
+        return await super.PATCH(endpoint, obj)
+    }
 
-//     async DELETE(endpoint, obj={headers:{},parameters:{}, body:{}}){
-//         return await super.DELETE(endpoint, obj)
-//     }
-// }
+    async DELETE(endpoint, obj={headers:{},parameters:{}, body:{}}){
+        return await super.DELETE(endpoint, obj)
+    }
+}
 
 
 
-// export { MockIOFetcher }
+export { MockIOFetcher }
--- test/MockIO.spec.mjs
+++ test/MockIO.spec.mjs
@@ -1,43 +1,43 @@
-// import {
-//     expectToBeTrue,
-//     expectValuesToMatch,
-//     expectValuesToEqual,
-//     expectArraysToBeEqual,
-//     expectArrayToInclude,
-//     expectArraytoIncludeArrayContents,
-//     objectsAreEquivalent,
-//     expectStringToInclude,
-//     expectObjectsAreEqual,
-//     expectToBeNull,
-//     throwsError,
-//     expectConstructorToThrowError
-// } from '../module/chaitests/Chai.mjs'
-// import {
-//     expect,
-//     should
-// } from 'chai'
-// import { MockIOFetcher as MockIO } from '../Components/Mockio.mjs'
+import {
+    expectToBeTrue,
+    expectValuesToMatch,
+    expectValuesToEqual,
+    expectArraysToBeEqual,
+    expectArrayToInclude,
+    expectArraytoIncludeArrayContents,
+    objectsAreEquivalent,
+    expectStringToInclude,
+    expectObjectsAreEqual,
+    expectToBeNull,
+    throwsError,
+    expectConstructorToThrowError
+} from '../module/chaitests/Chai.mjs'
+import {
+    expect,
+    should
+} from 'chai'
+import { MockIOFetcher as MockIO } from '../Components/Mockio.mjs'
 
 
 
 
 
-// const resourceLayers = {
-//     "user": {
-//         "id": { "type": "number" },
-//         "name": {
-//             "first": { "type":"string" },
-//             "last": { "type":"string" }
-//         },
-//         "task": { "type": "array" }
-//     },
-//     "task": {
-//         "task_id": { "type": "number" },
-//         "title": { "type":"string" },
-//         "completed": { "type":"boolean" }
-//     }
-// }
+const resourceLayers = {
+    "user": {
+        "id": { "type": "number" },
+        "name": {
+            "first": { "type":"string" },
+            "last": { "type":"string" }
+        },
+        "task": { "type": "array" }
+    },
+    "task": {
+        "task_id": { "type": "number" },
+        "title": { "type":"string" },
+        "completed": { "type":"boolean" }
+    }
+}
 
-// const m = new MockIO(resourceLayers)
-// console.log(m.resources)
+const m = new MockIO(resourceLayers)
+console.log(m.resources)
 
